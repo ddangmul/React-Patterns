7ec91e5bd53eafd71e40177b9ffabe5b6664b681
ddangmul <j.ddangmul@gmail.com>
2025-02-03 13:00:58 +0900

    context API, useState로 멀티 컴포넌트 상태 관리

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,3 +1,44 @@
+import { createContext, useState, useContext } from "react";
+
+// Accordion과 모든 연관된 컴포넌트를 다룰 컨텍스트라서 여기에 생성
+const AccordionContext = createContext();
+
+// 사용자 정의 훅 추가 생성 : 컨텍스트 읽기를 쉽고 안전하게 하기 위함
+export function useAccordionContext() {
+  // 컨텍스트 읽고 저장
+  const ctx = useContext(AccordionContext);
+
+  // 보안 강화 : 잘못된 곳에서 훅을 사용하면 에러 전달
+  if (!ctx) {
+    throw new Error(
+      "Accordion-related components must be wrapped by <Accordion>."
+    );
+  }
+
+  return ctx;
+}
+
 export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+  const [openItemId, setOpenItemId] = useState();
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+
+  function closeItem() {
+    setOpenItemId(null);
+  }
+
+  const contextValue = {
+    // 한번에 한가지 항목만 열리게 하므로 id 저장
+    openItemId: openItemId,
+    openItem,
+    closeItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
 }
--- src/components/accordion/AccordionItem.jsx
+++ src/components/accordion/AccordionItem.jsx
@@ -1,8 +1,28 @@
-export default function AccordionItem({ className, title, children }) {
+import { useAccordionContext } from "./Accordion";
+
+export default function AccordionItem({ id, className, title, children }) {
+  const { openItemId, openItem, closeItem } = useAccordionContext();
+
+  const isOpen = openItemId === id;
+
+  function handleClick() {
+    if (isOpen) {
+      closeItem(); // openItemId 상태값을 null로 설정
+    } else {
+      openItem(id); // openItemId 상태값을 해당 항목 id로 설정
+    }
+  }
+
   return (
     <li className={className}>
-      <h3>{title}</h3>
-      <div>{children}</div>
+      <h3 onClick={handleClick}>{title}</h3>
+      <div
+        className={
+          isOpen ? "accordion-item-content open" : "accordion-item-content"
+        }
+      >
+        {children}
+      </div>
     </li>
   );
 }
