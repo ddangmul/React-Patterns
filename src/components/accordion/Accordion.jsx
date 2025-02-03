23d20f01c98330223504331fe140e318ae8320cf
ddangmul <j.ddangmul@gmail.com>
2025-02-03 13:53:40 +0900

    컴파운드 컴포넌트 그룹화

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,5 +1,7 @@
 import { createContext, useState, useContext } from "react";
 
+import AccordionItem from "./AccordionItem";
+
 // Accordion과 모든 연관된 컴포넌트를 다룰 컨텍스트라서 여기에 생성
 const AccordionContext = createContext();
 
@@ -36,3 +38,7 @@ export default function Accordion({ children, className }) {
     </AccordionContext.Provider>
   );
 }
+
+// [컴파운드 컴포넌트 그룹화]
+// JS에서 함수는 곧 객체이므로 함수명(=객체명).속성명 으로 속성 추가
+Accordion.Item = AccordionItem;
