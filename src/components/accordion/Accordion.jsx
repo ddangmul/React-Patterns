19efe794401a06447db2fb9282fa3d494ef8b311
ddangmul <j.ddangmul@gmail.com>
2025-02-03 13:45:53 +0900

    상태 업데이트 함수를 toggleItem()으로 개선

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -21,19 +21,13 @@ export function useAccordionContext() {
 export default function Accordion({ children, className }) {
   const [openItemId, setOpenItemId] = useState();
 
-  function openItem(id) {
-    setOpenItemId(id);
-  }
-
-  function closeItem() {
-    setOpenItemId(null);
+  function toggleItem(id) {
+    setOpenItemId((prevId) => (prevId === id ? null : id));
   }
 
   const contextValue = {
-    // 한번에 한가지 항목만 열리게 하므로 id 저장
     openItemId: openItemId,
-    openItem,
-    closeItem,
+    toggleItem,
   };
 
   return (
--- src/components/accordion/AccordionItem.jsx
+++ src/components/accordion/AccordionItem.jsx
@@ -1,21 +1,13 @@
 import { useAccordionContext } from "./Accordion";
 
 export default function AccordionItem({ id, className, title, children }) {
-  const { openItemId, openItem, closeItem } = useAccordionContext();
+  const { openItemId, toggleItem } = useAccordionContext();
 
   const isOpen = openItemId === id;
 
-  function handleClick() {
-    if (isOpen) {
-      closeItem(); // openItemId 상태값을 null로 설정
-    } else {
-      openItem(id); // openItemId 상태값을 해당 항목 id로 설정
-    }
-  }
-
   return (
     <li className={className}>
-      <h3 onClick={handleClick}>{title}</h3>
+      <h3 onClick={() => toggleItem(id)}>{title}</h3>
       <div
         className={
           isOpen ? "accordion-item-content open" : "accordion-item-content"
